feb0e1b16f297726a00328ec8f6c40273ec8b16f
Tushar <tusharverma02001@gmail.com>
2026-09-19 02:59:11 +0530

    Connect frontend to Render backend

--- src/api/AxiosConfig.js
+++ src/api/AxiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8083",
+    baseURL: "https://odmanagerbackend-3.onrender.com",
 });
 
 export default api;
+
